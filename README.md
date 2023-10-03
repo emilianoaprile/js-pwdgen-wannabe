f70ef1e708a1dfa92eb12380588744ee789f8fd8
EmilianoAprile <emilianoaprile77@icloud.com>
2023-10-03 15:44:10 +0200

    First commit - added README + js code

---
+++ README.md
@@ -0,0 +1,26 @@
+# Password Generator
+
+### Chiedere il nome all'utente:
+    - dichiarare una variabile "nome"
+    - assegnare alla variabile il valore restituito da un prompt
+
+### Chiedere il cognome all'utente:
+    - dichiarare una variabile "cognome"
+    - asseganre alla variabile il valore restituito da un prompt
+
+### Chiedere il colore preferito all'utente:
+    - dichiarare una variabile "colore preferito"
+    - assegnare alla variabile il valore restituito da un prompt
+
+## Decidere un numero da inserire nella password:
+    - dichiarare una variabile "numero"
+    - asseganre alla variabile il valore del numero scelto
+
+## Risultato: concatenare la password 
+    - concatenare le variabili "nome" + "cognome" + "colore preferito" + "numero"
+    - assegnare una variabile con le variabili concatenate "userPassword"
+
+## Stampare il risultato sul browser:
+    - inserire nell'html un tag con id="password"
+    - recuperare con js l'elemento con id="password"
+    - modificare l'innerHTML del tag con il valore della variabile "userPassword"
---
+++ js/app.js
@@ -0,0 +1,41 @@
+// # Password Generator
+
+// ### Chiedere il nome all'utente:
+//     - dichiarare una variabile "nome"
+        let userName;
+//     - assegnare alla variabile il valore restituito da un prompt
+        userName = prompt ('Ciao, inserisci il tuo nome!');
+        console.log (userName);
+
+// ### Chiedere il cognome all'utente:
+//     - dichiarare una variabile "cognome"
+        let userSurname;
+//     - asseganre alla variabile il valore restituito da un prompt
+        userSurname = prompt ('Perfetto, ora inserisci il tuo cognome!');
+        console.log (userSurname);
+
+// ### Chiedere il colore preferito all'utente:
+//     - dichiarare una variabile "colore preferito"
+        let userColor;
+//     - assegnare alla variabile il valore restituito da un prompt
+        userColor = prompt ('Ottimo, adesso inserisci il tuo colore preferito!');
+        console.log (userColor);
+
+// ## Decidere un numero da inserire nella password:
+//     - dichiarare una variabile "numero"
+        let pswNumber;
+//     - asseganre alla variabile il valore del numero scelto
+        pswNumber = '21';
+        console.log (pswNumber);
+
+// ## Risultato: concatenare la password 
+//     - concatenare le variabili "nome" + "cognome" + "colore preferito" + "numero"
+        let userPassword;
+//     - assegnare una variabile con le variabili concatenate "userPassword"
+        userPassword = userName + userSurname + userColor + pswNumber;
+        console.log (userPassword);
+
+// ## Stampare il risultato sul browser:
+//     - inserire nell'html un tag con id="password"
+//     - recuperare con js l'elemento con id="password"
+//     - modificare l'innerHTML del tag con il valore della variabile "userPassword"
